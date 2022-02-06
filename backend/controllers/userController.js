1eb4b57879dfd695aa45002160052a571da190dc
Sandeep Shrestha <58897416+sandeepShr1@users.noreply.github.com>
2022-02-06 13:38:02 +0545

    reset password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,8 @@
 const ErrorHandler = require("../utils/errorHandler")
 const catchAsyncError = require("../middleware/catchAsyncErrors");
 const User = require("../model/userModel");
-const sendToken = require("../utils/generateToken")
-
+const sendToken = require("../utils/generateToken");
+const sendEmail = require("../utils/sendEmail");
 
 // Registering User
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -57,3 +57,43 @@ exports.logout = catchAsyncError(async (req, res, next) => {
             message: "Logged out",
       });
 });
+
+// Forgot password
+
+exports.forgotPassword = catchAsyncError(async (req, res, next) => {
+      const user = await User.findOne({ email: req.body.email });
+
+      if (!user) {
+            return next(new ErrorHandler("User not found", 404));
+      }
+
+      // Get ResetPassword Token
+      const resetToken = user.getResetPasswordToken();
+      await user.save({ validateBeforeSave: false });
+
+      const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+      const message = `Your password reset token is:- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it`;
+
+      try {
+            await sendEmail({
+                  email: user.email,
+                  subject: `Ecommerce Password Recovery `,
+                  message,
+            })
+            res.status(200).json({
+                  success: true,
+                  message: `Email sent to ${user.email} successfully`
+            });
+
+      } catch (error) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+
+            await user.save({ validateBeforeSave: false });
+
+            return next(new ErrorHandler(
+                  error.message, 500
+            ))
+      }
+})
