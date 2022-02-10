eb3eddc69665ce6f34e90296e9eaa77aab02142d
Sandeep Shrestha <58897416+sandeepShr1@users.noreply.github.com>
2022-02-10 21:56:06 +0545

    fetching data with api init

--- frontend/src/components/Home/Product.js
+++ frontend/src/components/Home/Product.js
@@ -8,18 +8,18 @@ const Product = ({ product }) => {
             edit: false,
             color: "rgba(20,20,20,0.2)",
             activeColor: "tomato",
-            size: window.innerWidth < 600 ? 20 : 25,
-            value: product.rating,
+            size: window.innerWidth < 900 ? 20 : 25,
+            value: product.ratings,
             isHalf: true
       }
       return (
             <Link className='productCard' to={product._id}>
-                  {/* <img src={product.images[0].url} alt={product.name} /> */}
+                  <img src={product.images[0].url} alt={product.name} />
                   <p>{product.name}</p>
                   <div>
                         <ReactStarts {...options} /> <span>({product.numOfReviews} Reviews)</span>
                   </div>
-                  <span>{product.price}</span>
+                  <span>Rs.{product.price}</span>
             </Link>
       )
 }
