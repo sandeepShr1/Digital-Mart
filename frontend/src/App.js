a6fa33c8374aba19d066d7f736170ea24c2cccbf
Sandeep Shrestha <58897416+sandeepShr1@users.noreply.github.com>
2022-02-10 13:32:19 +0545

    frontend started and completed header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
 import './App.css';
-import Header from "./components/layout/Header.js";
-import { BrowserRouter as Router } from "react-router-dom"
-
+import Header from "./components/layout/Header/Header.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WebFont from 'webfontloader';
+import Footer from './components/layout/Footer/Footer';
+import Home from "./components/Home/Home.js"
+import Products from "./components/Products/Products.js"
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    })
+  }, [])
   return <Router>
     <Header />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/products" element={<Products />} />
+    </Routes>
+    <Footer />
   </Router>
 }
 
