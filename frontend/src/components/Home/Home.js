c97bb696cd7d52f251bd655b2f961c9611c9c387
Sandeep Shrestha <58897416+sandeepShr1@users.noreply.github.com>
2022-02-10 15:54:11 +0545

    completed product cart

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { CgMouse } from "react-icons/all";
+import './Home.css';
+import Product from "./Product.js";
 
+const product = {
+      name: "blue t-shirt",
+      images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+      price: "Rs. 400",
+      _id: "sandeep"
+}
 const Home = () => {
       return (
-            <div>Home</div>
+            <>
+                  <div className="banner">
+                        <p>Welcome to Ecommerce</p>
+                        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+                        <a href="#container">
+                              <button>
+                                    Scroll <CgMouse />
+                              </button>
+                        </a>
+                  </div>
+
+                  <h2 className="homeHeading">Features Products</h2>
+                  <div className="container" id='container'>
+                        <Product product={product} />
+                        <Product product={product} />
+                        <Product product={product} />
+                        <Product product={product} />
+                        <Product product={product} />
+                        <Product product={product} />
+                        <Product product={product} />
+                        <Product product={product} />
+                  </div>
+            </>
       )
 }
 
