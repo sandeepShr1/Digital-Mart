3117f092efdc0549bfe1f6bdd1b5c0f0ed46522d
Sandeep Shrestha <58897416+sandeepShr1@users.noreply.github.com>
2022-06-23 13:00:22 +0545

    adding some lazy loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import Footer from './components/layout/Footer/Footer';
+
 import Home from "./components/Home/Home"
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ProductDetails from "./components/Product/ProductDetails"
-import Products from "./components/Product/Products"
+// import ProductDetails from "./components/Product/ProductDetails"
+// import Products from "./components/Product/Products";
 import LoginSignup from './components/User/LoginSignup';
 import store from "./redux/store";
 import { loadUser } from './redux/actions/userActions';
@@ -23,7 +24,7 @@ import Success from "./components/Cart/Success"
 import MyOrders from "./components/Order/MyOrders"
 import OrderDetails from "./components/Order/OrderDetails"
 import Dashboard from "./components/Admin/Dashboard";
-import ProductList from "./components/Admin/ProductList";
+// import ProductList from "./components/Admin/ProductList";
 import Login from "./components/User/LoginSignup";
 import NewProduct from './components/Admin/NewProduct';
 import UpdateProduct from './components/Admin/UpdateProduct';
@@ -40,6 +41,12 @@ import ValidatePayment from './components/Cart/ValidatePayment';
 import Banners from './components/Admin/Banner/Banners';
 import NewBanner from './components/Admin/Banner/NewBanner';
 import UpdateBanner from './components/Admin/Banner/UpdateBanner';
+import Loader from './components/layout/Loader/Loader';
+// const Home = React.lazy(() => import("./components/Home/Home"));
+const Products = React.lazy(() => import("./components/Product/Products"));
+const ProductDetails = React.lazy(() => import("./components/Product/ProductDetails"));
+// const Dashboard = React.lazy(() => import("./components/Admin/Dashboard"));
+const ProductList = React.lazy(() => import("./components/Admin/ProductList"));
 
 function App() {
 
@@ -55,52 +62,53 @@ function App() {
 
   return <Router>
     <Navbar />
-    {isAuthenticated && <UserOptions user={user} />}
+    <Suspense fallback={<Loader />}>
+      {isAuthenticated && <UserOptions user={user} />}
+      <div className="main-container">
+        <Routes>
+          <Route path='*' element={<NotFound />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/product/:id" element={<ProductDetails />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/about" element={<About />} />
+          <Route path="/products/:keyword" element={<Products />} />
+          <Route exact path="/login" element={<LoginSignup />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route path="/password/forgot" element={<ForgotPassword />} />
+          <Route path="/password/reset/:token" element={<ResetPassword />} />
+          <Route path="/account" element={isAuthenticated ? <Profile /> : <Login />} />
+          <Route path="/me/update" element={isAuthenticated ? <UpdateProfile /> : <Login />} />
+          <Route path="/password/update" element={isAuthenticated ? <UpdatePassword /> : <Login />} />
+          <Route path="/shipping" element={isAuthenticated ? <Shipping /> : <Login />} />
+          <Route path="/order/confirm" element={isAuthenticated ? <ConfirmOrder /> : <Login />} />
+          <Route path="/process/payment" element={isAuthenticated ? <Payment /> : <Login />} />
+          <Route path="/epay/transrec" element={isAuthenticated ? <ValidatePayment /> : <Login />} />
+          <Route path="/success" element={isAuthenticated ? <Success /> : <Login />} />
+          <Route path="/failed" element={isAuthenticated ? <Failed /> : <Login />} />
+          <Route path="/orders" element={isAuthenticated ? <MyOrders /> : <Login />} />
+          <Route path="/order/:id" element={isAuthenticated ? <OrderDetails /> : <Login />} />
 
-    <div className="main-container">
-      <Routes>
-        <Route path='*' element={<NotFound />} />
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/product/:id" element={<ProductDetails />} />
-        <Route exact path="/products" element={<Products />} />
-        <Route exact path="/about" element={<About />} />
-        <Route path="/products/:keyword" element={<Products />} />
-        <Route exact path="/login" element={<LoginSignup />} />
-        <Route exact path="/cart" element={<Cart />} />
-        <Route path="/password/forgot" element={<ForgotPassword />} />
-        <Route path="/password/reset/:token" element={<ResetPassword />} />
-        <Route path="/account" element={isAuthenticated ? <Profile /> : <Login />} />
-        <Route path="/me/update" element={isAuthenticated ? <UpdateProfile /> : <Login />} />
-        <Route path="/password/update" element={isAuthenticated ? <UpdatePassword /> : <Login />} />
-        <Route path="/shipping" element={isAuthenticated ? <Shipping /> : <Login />} />
-        <Route path="/order/confirm" element={isAuthenticated ? <ConfirmOrder /> : <Login />} />
-        <Route path="/process/payment" element={isAuthenticated ? <Payment /> : <Login />} />
-        <Route path="/epay/transrec" element={isAuthenticated ? <ValidatePayment /> : <Login />} />
-        <Route path="/success" element={isAuthenticated ? <Success /> : <Login />} />
-        <Route path="/failed" element={isAuthenticated ? <Failed /> : <Login />} />
-        <Route path="/orders" element={isAuthenticated ? <MyOrders /> : <Login />} />
-        <Route path="/order/:id" element={isAuthenticated ? <OrderDetails /> : <Login />} />
-
-        {/* <Route element={<ProtectedRoute isAuthenticated={isAuthenticated } isAdmin={true} adminRoute={true} />}>
+          {/* <Route element={<ProtectedRoute isAuthenticated={isAuthenticated } isAdmin={true} adminRoute={true} />}>
         <Route path="/admin/dashboard" element={<Dashboard />} />
       </Route> */}
 
-        <Route path="/admin/dashboard" element={isAuthenticated && user.role === "admin" ? <Dashboard /> : <Login />} />
-        <Route path="/admin/products" element={isAuthenticated && user.role === "admin" ? <ProductList /> : <Login />} />
-        <Route path="/admin/product/" element={isAuthenticated && user.role === "admin" ? <NewProduct /> : <Login />} />
-        <Route path="/admin/product/:id" element={isAuthenticated && user.role === "admin" ? <UpdateProduct /> : <Login />} />
-        <Route path="/admin/orders" element={isAuthenticated && user.role === "admin" ? <OrderList /> : <Login />} />
-        <Route path="/admin/order/:id" element={isAuthenticated && user.role === "admin" ? <UpdateOrder /> : <Login />} />
-        <Route path="/admin/users" element={isAuthenticated && user.role === "admin" ? <Users /> : <Login />} />
-        <Route path="/admin/user/:id" element={isAuthenticated && user.role === "admin" ? <UpdateUser /> : <Login />} />
-        <Route path="/admin/reviews" element={isAuthenticated && user.role === "admin" ? <Reviews /> : <Login />} />
+          <Route path="/admin/dashboard" element={isAuthenticated && user.role === "admin" ? <Dashboard /> : <Login />} />
+          <Route path="/admin/products" element={isAuthenticated && user.role === "admin" ? <ProductList /> : <Login />} />
+          <Route path="/admin/product/" element={isAuthenticated && user.role === "admin" ? <NewProduct /> : <Login />} />
+          <Route path="/admin/product/:id" element={isAuthenticated && user.role === "admin" ? <UpdateProduct /> : <Login />} />
+          <Route path="/admin/orders" element={isAuthenticated && user.role === "admin" ? <OrderList /> : <Login />} />
+          <Route path="/admin/order/:id" element={isAuthenticated && user.role === "admin" ? <UpdateOrder /> : <Login />} />
+          <Route path="/admin/users" element={isAuthenticated && user.role === "admin" ? <Users /> : <Login />} />
+          <Route path="/admin/user/:id" element={isAuthenticated && user.role === "admin" ? <UpdateUser /> : <Login />} />
+          <Route path="/admin/reviews" element={isAuthenticated && user.role === "admin" ? <Reviews /> : <Login />} />
 
-        <Route path="/admin/banners" element={isAuthenticated && user.role === "admin" ? <Banners /> : <Login />} />
-        <Route path="/admin/banner/" element={isAuthenticated && user.role === "admin" ? <NewBanner /> : <Login />} />
-        <Route path="/admin/banner/:id" element={isAuthenticated && user.role === "admin" ? <UpdateBanner /> : <Login />} />
-      </Routes>
-    </div>
-    <Footer />
+          <Route path="/admin/banners" element={isAuthenticated && user.role === "admin" ? <Banners /> : <Login />} />
+          <Route path="/admin/banner/" element={isAuthenticated && user.role === "admin" ? <NewBanner /> : <Login />} />
+          <Route path="/admin/banner/:id" element={isAuthenticated && user.role === "admin" ? <UpdateBanner /> : <Login />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Suspense>
   </Router>
 
 }
--- frontend/src/components/Home/FooterBanner.jsx
+++ frontend/src/components/Home/FooterBanner.jsx
@@ -1,33 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./FooterBanner.css";
-import footerBanner from "../../images/headphones_c_1.webp"
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react';
+import { clearError, getBanner } from "../../redux/actions/bannerActions"
 
 const FooterBanner = () => {
+      const { banners, error } = useSelector(state => state.banners);
+      const dispatch = useDispatch();
+
+      useEffect(() => {
+            if (error) {
+                  alert.error(error);
+                  dispatch(clearError());
+            }
+
+            dispatch(getBanner());
+      }, [dispatch, error]);
+
+
       return (
             <div className="footer-banner-container">
                   <div className="banner-desc">
                         <div className="left">
-                              <p>20% off</p>
-                              <h3>Grab Now</h3>
-                              <h3>Good</h3>
+                              <p>{banners[0]?.discount} % off.</p>
+                              <h3>{banners[0]?.buttonText}</h3>
+                              <h3>{banners[0]?.largeText1}</h3>
                               <p>Up to March</p>
                         </div>
                         <div className="right">
                               <p>Hurry Up</p>
-                              <h3>Stylish</h3>
-                              <p>Lorem ipsum.</p>
+                              <h3>{banners[0]?.largeText2}</h3>
+                              <p>{banners[0]?.desc}</p>
                               <Link to="/product">
-                                    <button type="button">Buy Now</button>
+                                    <button type="button">{banners[0]?.buttonText}</button>
                               </Link>
                         </div>
 
                         <img
-                              src={footerBanner} className="footer-banner-image" alt="footerImg"
+                              src={banners[0]?.images[0]?.url} className="footer-banner-image" alt="footerImg"
                         />
                   </div>
             </div>
       )
 }
 
-export default FooterBanner
+export default FooterBanner;
